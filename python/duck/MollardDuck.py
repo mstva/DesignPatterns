4243d38fa8a1cf5df31ee098075927fe191c3aef
mstava <mustafaabdo468@gmail.com>
2020-12-06 23:01:07 +0200

    deleted previous duck python code

--- python/duck/MollardDuck.py
+++
@@ -1,6 +0,0 @@
-from Duck import Duck
-
-
-class MollardDuck(Duck):
-    """this is a mollard duck class, a subclass from Duck"""
-    pass
--- python/duck/RedheadDuck.py
+++
@@ -1,6 +0,0 @@
-from Duck import Duck
-
-
-class RedheadDuck(Duck):
-    """this is a  redhead duck class, a subclass from Duck"""
-    pass
